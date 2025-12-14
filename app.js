141235ef8d2d702a61b21d9d9220adf71e25eb60
sankalp khare <kharesankalp57@gmail.com>
2025-12-14 20:33:05 +0530

    Created Update Route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride=require("method-override")
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -20,6 +21,7 @@ async function main(){
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"))
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 app.get("/", (req,res) =>{
     res.send("Hi, I am Root");
@@ -59,6 +61,14 @@ app.get("/listings/:id/edit",async (req,res)=>{
     const listing= await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
 })
+
+//Update Route
+app.put("/listing/:id",async (req,res)=>{
+        let {id}=req.params;
+       await Listing.findByIdAndUpdate(id,{...req.body.listing});
+        res.redirect("/listings");
+
+});
 
 
 
